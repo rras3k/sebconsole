c1f8c5da3f254f54dac7690e7a624c6af852b654
rras3k <89064217+rras3k@users.noreply.github.com>
2022-12-06 14:08:53 +0100

    maj

--- ressources/views/layouts/app.blade.php
+++ ressources/views/layouts/app.blade.php
@@ -28,12 +28,7 @@ use Illuminate\Support\Facades\Auth;
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
 
-    {{-- <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css"
-        integrity="sha512-5PV92qsds/16vyYIJo3T/As4m2d8b6oWYfoqV+vtizRB6KhF1F9kYzWzQmsO6T3z3QG2Xdhrx7FQ+5R1LiQdUA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
-
-    <link rel="stylesheet"
+<link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.2/font/bootstrap-icons.min.css"
         integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
