54dd234c03cbef80db2ffa44264560a053037459
rras3k <89064217+rras3k@users.noreply.github.com>
2023-01-03 10:57:23 +0100

    ajout test si null

--- ressources/views/components/menus/page.blade.php
+++ ressources/views/components/menus/page.blade.php
@@ -1,15 +1,18 @@
-    {{-- 
+    {{--
     liste[]['class']
     liste[]['url']
     liste[]['classIcon']
-    liste[]['titre'] 
+    liste[]['titre']
     --}}
-    @foreach ($liste as $elt)
-        @if (!isset($elt['class']))
-            <?php $elt['class'] = ''; ?>
-        @endif
-        <a href="{{ $elt['url'] }}" role="button"
-            class="btn btn-sm btn-outline-secondary me-1 mt-1 {{ $elt['class'] }}"><i
-                class="{{ $elt['classIcon'] }}"></i>
-            {{ $elt['titre'] }}</a>
-    @endforeach
+    @if ($liste != null)
+
+        @foreach ($liste as $elt)
+            @if (!isset($elt['class']))
+                <?php $elt['class'] = ''; ?>
+            @endif
+            <a href="{{ $elt['url'] }}" role="button"
+                class="btn btn-sm btn-outline-secondary me-1 mt-1 {{ $elt['class'] }}"><i
+                    class="{{ $elt['classIcon'] }}"></i>
+                {{ $elt['titre'] }}</a>
+        @endforeach
+    @endif
