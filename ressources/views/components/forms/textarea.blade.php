cc7ca0b38d5cb3a9c953ab830e3d8982034fec4b
rras3k <89064217+rras3k@users.noreply.github.com>
2023-01-14 06:52:24 +0100

    ajout filtre permanent

--- ressources/views/components/forms/textarea.blade.php
+++ ressources/views/components/forms/textarea.blade.php
@@ -1,7 +1,31 @@
-<div class="col-{{ $col }} position-relative" id="entree-{{ $nom }}">
+{{-- <div class="col-{{ $col }} position-relative" id="entree-{{ $nom }}">
     <label for="input-{{ $nom }}" class="form-label label_form">{{ $label }}</label>
     <textarea class="form-control  @error($nom) is-invalid @enderror " id="input-{{ $nom }}"
         rows="{{ $rows }}" placeholder="{{ $placeholder }}" name="{{ $nom }}">{{ $value }}</textarea>
+    @error($nom)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
+</div> --}}
+
+@props([
+    'col' => 12,
+    'rows' => 10,
+    'readonly'=> false,
+    'nom',
+    'label',
+    'value',
+    'readonly'=> isset($readonly) && $readonly=="true" ? true : false,
+    'required' => isset($required) && $required=="false" ? false : true,
+    'placeholder'=> "fefdsfds ",
+])
+<div class="form-floating" id="entree-{{ $nom }}"" style="height: 100%;" >
+    <textarea class="form-control  @error($nom) is-invalid @enderror " id="input-{{ $nom }}" "
+     placeholder="{{ $placeholder }}" name="{{ $nom }}" style="height: 100%;">{{ $value }}</textarea>
+
+    <label for="input-{{ $nom }}" class="form-label label_form">{{ $label }}</label>
+
     @error($nom)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
