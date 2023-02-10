45770256a11bd93511a128ef579fc66021db8483
rras3k <89064217+rras3k@users.noreply.github.com>
2023-02-10 11:14:20 +0100

    maj

---
+++ src/Models/SbModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rras3k\Sebconsole\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class SbModel extends Model
+{
+	use HasFactory;
+
+	abstract public static function getStrName(); // retourne le champ de la désignation lisable de l'enregistrement
+	abstract public static function getLabel(); // retourne le libelle de la table
+	abstract public static function getList(); // retourne la liste des enregistrements id, ... as label
+
+
+}
