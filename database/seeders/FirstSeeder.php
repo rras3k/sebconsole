2f4bafbbb193f65edcd466e6506035c51f7e9b98
rras3k <89064217+rras3k@users.noreply.github.com>
2023-02-12 18:08:11 +0100

    fix

--- database/seeders/FirstSeeder.php
+++ database/seeders/FirstSeeder.php
@@ -32,13 +32,8 @@ class FirstSeeder extends Seeder
 
         // roles
         DB::table('roles')->truncate();
-        DB::table('roles')->insert(['id'=>1,'nom' => 'root', 'fonction' => 'root']);
-        DB::table('roles')->insert(['id'=>2,'nom' =>'admin', 'fonction' => 'root']);
-        DB::table('roles')->insert(['id'=>50,'nom' =>'member1', 'fonction' => 'member1']);
-        DB::table('roles')->insert(['id'=>51,'nom' =>'member2', 'fonction' => 'member2']);
-        DB::table('roles')->insert(['id'=>52,'nom' =>'member3', 'fonction' => 'member3']);
-        DB::table('roles')->insert(['id'=>53,'nom' =>'member4', 'fonction' => 'member4']);
-        DB::table('roles')->insert(['id'=>54,'nom' =>'member5', 'fonction' => 'member5']);
+        DB::table('roles')->insert(['id' => 1, 'nom' => 'root', 'fonction' => 'root']);
+        DB::table('roles')->insert(['id' => 2, 'nom' => 'admin', 'fonction' => 'root']);
 
 
         // role_user
@@ -49,9 +44,7 @@ class FirstSeeder extends Seeder
         // log_types
         DB::table('log_types')->truncate();
         DB::table('log_types')->insert(
-            ['id' => 1, 'nom' => 'login accepted','sys'=>true]
+            ['id' => 1, 'nom' => 'login accepted', 'sys' => true]
         );
-        
     }
 }
-
