7f41db93b87f444d18c032d776caa6a927aa8384
rras3k <89064217+rras3k@users.noreply.github.com>
2023-02-14 15:24:18 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,29 @@
 use Illuminate\Support\Facades\Route;
 // use Rras3k\Sebconsole\Models\Role;
 
-use App\Models\Role;
+// use App\Models\Role;
 
 
-Route::group(
-    [
-        'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
-        'middleware' => ['web', 'auth', 'role:' . Role::ADMIN]
-    ],
-    function () {
-        // Pages Pasgit
-        Route::get('rras3k/pages-pasgit', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'index'])->name('page-pasgit.index');
-        Route::get('rras3k/pages-pasgit/page/{nompage}', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'getPage']);
-    }
-);
+// Route::group(
+//     [
+//         'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
+//         'middleware' => ['web', 'auth', 'role:' . Role::ADMIN]
+//     ],
+//     function () {
+//         // Pages Pasgit
+//         Route::get('rras3k/pages-pasgit', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'index'])->name('page-pasgit.index');
+//         Route::get('rras3k/pages-pasgit/page/{nompage}', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'getPage']);
+//     }
+// );
 
-Route::group(
-    [
-        'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
-        'middleware' => ['web', 'auth', 'role:' . Role::ROOT]
-    ],
-    function () {
-        Route::get('rras3k/genere-mvc', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'show'])->name('genereMvc.show');
-        Route::get('rras3k/genere-check', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'check'])->name('genereMvc.check');
-        Route::post('rras3k/genere-run', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'run'])->name('genereMvc.run');
-    }
-);
+// Route::group(
+//     [
+//         'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
+//         'middleware' => ['web', 'auth', 'role:' . Role::ROOT]
+//     ],
+//     function () {
+//         Route::get('rras3k/genere-mvc', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'show'])->name('genereMvc.show');
+//         Route::get('rras3k/genere-check', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'check'])->name('genereMvc.check');
+//         Route::post('rras3k/genere-run', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'run'])->name('genereMvc.run');
+//     }
+// );
--- toCopy/routes/web.php
+++ toCopy/routes/web.php
@@ -30,6 +30,30 @@ Route::group(
     }
 );
 
+Route::group(
+    [
+        'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
+        'middleware' => ['web', 'auth', 'role:' . Role::ADMIN]
+    ],
+    function () {
+        // Pages Pasgit
+        Route::get('rras3k/pages-pasgit', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'index'])->name('page-pasgit.index');
+        Route::get('rras3k/pages-pasgit/page/{nompage}', [Rras3k\Sebconsole\Http\Controllers\PagePasgitController::class, 'getPage']);
+    }
+);
+
+Route::group(
+    [
+        'namespace' => 'Rras3k\Sebconsole\Http\Controllers',
+        'middleware' => ['web', 'auth', 'role:' . Role::ROOT]
+    ],
+    function () {
+        Route::get('rras3k/genere-mvc', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'show'])->name('genereMvc.show');
+        Route::get('rras3k/genere-check', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'check'])->name('genereMvc.check');
+        Route::post('rras3k/genere-run', [\Rras3k\Sebconsole\Http\Controllers\genereMvcController::class, 'run'])->name('genereMvc.run');
+    }
+);
+
 //------------------------------- ROOT
 Route::middleware(['auth', 'role:' . Role::ROOT])->group(function () {
    
