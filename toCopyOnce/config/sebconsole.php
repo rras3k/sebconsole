1be1831d2d775cde39aa9a5bc5b09779841f913b
rras3k <89064217+rras3k@users.noreply.github.com>
2023-03-02 14:55:31 +0100

    maj

--- toCopyOnce/config/sebconsole.php
+++ toCopyOnce/config/sebconsole.php
@@ -5,24 +5,10 @@ use App\Models\Role;
 
 return [
     'affiche_logo' => true,         // Affichage du logo /public/img/logo.png
-    'affiche_icon_libre' => true,  // Affichage des icons des menus de façon inline
-    'menu_accordeon' => true,      // Affichage des rubriques avec accordeon
-    'url_doc' => "",
-    'menu' => [ // Menu de la sidebar. Role::ROOT, Role::ADMIN, Role::MEMBRE_1, Role::MEMBRE_2, ...
+    'menu_affichage_liste_icons' => 'liste_icons_1',
+    'menu_affichage_vertical' => 'vertical_1',
+    'menu_affichage_horizontal' => 'horizontal_1',
+    'menu' => [
 
-        // System
-        ['rubrique' => 'Système', 'nom' => 'Génération MVC', 'route' => 'genereMvc.show', 'icon' => 'bi bi-person', 'droits' => [Role::ROOT]],
-
-
-        
-
-        // dev
-        ['rubrique' => 'Dev', 'nom' => 'Pages dev', 'route' => 'page-pasgit.index', 'icon' => 'fa-solid fa-city', 'droits' => [Role::ADMIN]],
-
-
-
-
-
-
-    ],
+    ]
 ];
