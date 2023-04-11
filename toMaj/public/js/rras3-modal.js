611fae36f7e4d6dfda88e0e45ba80de1730eb53a
rras3k <89064217+rras3k@users.noreply.github.com>
2023-04-11 16:53:43 +0200

    maj

--- toMaj/public/js/rras3-modal.js
+++ toMaj/public/js/rras3-modal.js
@@ -1,4 +1,42 @@
 class rras3k_modal {
+    modalHtmlId = null
+    mode = null
+
+    constructor(modalHtmlId, mode = null) {
+        this.modalHtmlId = modalHtmlId
+        this.mode = mode
+        this.clean()
+    }
+
+    clean() {
+        document.querySelector('#' + this.modalHtmlId + ' .modal-title').innerHTML = ''     // efface le titre
+        document.querySelector('#' + this.modalHtmlId + ' .modal-body').innerHTML = ''      // efface le corps
+        document.querySelector('#' + this.modalHtmlId + ' .modal-footer').innerHTML = ''    // efface le footer
+    }
+
+    corps(text) {
+        document.querySelector('#' + this.modalHtmlId + ' .modal-body').innerHTML = text
+    }
+
+    footer_button_add(btns) {
+        var eltFooter = document.querySelector('#' + this.modalHtmlId + ' .modal-footer')
+        var tmp = ''
+        btns.forEach(element => {
+            element.onclick = element.onclick ? ' onclick=' + element.onclick + '() ' : ''
+            console.log(element.label)
+            tmp += '<button type="button" ' + element.onclick + ' class="btn ' + element.class + '" data-bs-dismiss="modal">' + element.label + '</button>'
+        });
+        eltFooter.innerHTML = tmp
+    }
+    header(titre) {
+        document.querySelector('#' + this.modalHtmlId + ' .modal-title').innerHTML = titre
+
+    }
+
+
+    show() {
+        var mModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+        mModal.show()
+    }
 
 }
-
